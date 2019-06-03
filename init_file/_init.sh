290040604319511f3143b716b2a654d61507e348
kinzun <mail.kinzu@gmail.com>
2019-06-03 11:33:02 +0800

    test(init_file): add func

--- init_file/_init.sh
+++ init_file/_init.sh
@@ -2,20 +2,23 @@
 
 
 
-
-
 echo "---------------------------------------------------------------------------------------------------------------------"
 
-
+function bashrc_config() {
+egrep 'LANG="en_US.UTF-8"' /etc/bashrc >/dev/null
+if [ $? -ne 0 ]; then
 cat << EOF >> /etc/bashrc
 export LANG="en_US.UTF-8"
 EOF
+fi
+}
+limits_config
 
 yum install -y bash-completion && yum install -y bash-completion-extras.noarch
 yum install -y git
 
 
-yum install python-setuptools && easy_install pip
+yum install -y python-setuptools && easy_install pip
 pip install git+https://github.com/shadowsocks/shadowsocks.git@master
 
 echo "init finish"
